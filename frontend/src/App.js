abc4dc098a8c8d565a36b5653704398c370fca17
truong <jason.nixathor@obello.com>
2023-11-12 16:29:27 +0700

    Design UI for each section of homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,30 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AppLayout from "./Layouts/AppLayout";
 import "./App.css";
-import AdminRoute from './Route/AdminRoute';
+import AdminRoute from "./Route/AdminRoute";
 import Home from "./Pages/Home/Home";
-import Hotels from "./Pages/Hotels/Hotels";
-import News from "./Pages/News/News";
-import PrevFilterContext  from "./context/PrevFilterContext";
-import DetailSite from "./Pages/DetailSite";
-import DetailNews from "./Pages/DetailNews";
-import DetailHotel from "./Pages/DetailHotel";
-import DetailRoom from "./Pages/DetailRoom";
-import Signin from './Pages/Signin';
-import Signup from './Pages/Signup'
+// import Hotels from "./Pages/Hotels/Hotels";
+// import News from "./Pages/News/News";
+import PrevFilterContext from "./context/PrevFilterContext";
+// import DetailSite from "./Pages/DetailSite";
+// import DetailNews from "./Pages/DetailNews";
+// import DetailHotel from "./Pages/DetailHotel";
+// import DetailRoom from "./Pages/DetailRoom";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
 import LogOut from "./Pages/Logout";
-import Payment from "./Pages/Payment";
-import ThankYou from "./Pages/Thankyou";
-import AccountSetting from "./Pages/AccountSetting";
-import AccountBooking from "./Pages/AccountSetting/AccountBooking";
-import AccountMemory from "./Pages/AccountSetting/AccountMemory";
-import SearchResult from "./Pages/SearchResult";
-import PageNotFound from './Pages/PageNotFound';
+// import Payment from "./Pages/Payment";
+// import ThankYou from "./Pages/Thankyou";
+import PageNotFound from "./Pages/PageNotFound";
 
-import Dashboard from './Pages/Admin/Dashboard';
-import MainLayout from './Components/Admin/MainLayout';
-import Room from './Pages/Admin/RoomAdmin/Room';
-import Hotel from './Pages/Admin/HotelAdmin/Hotel';
-import Blog from './Pages/Admin/BlogAdmin/Blog';
-import User from './Pages/Admin/UserAdmin/User';
-import Order from './Pages/Admin/OrderAdmin/Order';
-import OrderDetail from './Pages/Admin/OrderAdmin/OrderDetail'
+import Dashboard from "./Pages/Admin/Dashboard";
+import MainLayout from "./Components/Admin/MainLayout";
+import Room from "./Pages/Admin/RoomAdmin/Room";
+import Hotel from "./Pages/Admin/HotelAdmin/Hotel";
+import Blog from "./Pages/Admin/BlogAdmin/Blog";
+import User from "./Pages/Admin/UserAdmin/User";
+import Order from "./Pages/Admin/OrderAdmin/Order";
+import OrderDetail from "./Pages/Admin/OrderAdmin/OrderDetail";
 
 function App() {
   return (
@@ -42,7 +38,7 @@ function App() {
                   <Home />
                 </AppLayout>
               </Route>
-              <Route path="/search" exact>
+              {/* <Route path="/search" exact>
                   <AppLayout>
                      <SearchResult />
                   </AppLayout>
@@ -56,8 +52,8 @@ function App() {
                   <AppLayout>
                     <DetailHotel />
                   </AppLayout>
-              </Route>
-              <Route path="/news" exact>
+              </Route> */}
+              {/* <Route path="/news" exact>
                 <AppLayout>
                   <News />
                 </AppLayout>
@@ -101,10 +97,10 @@ function App() {
                   <AppLayout>
                     <AccountSetting />
                   </AppLayout>
-              </Route>
+              </Route> */}
               <AdminRoute path="/dashboard" exact>
                 <MainLayout>
-                  <Dashboard/>
+                  <Dashboard />
                 </MainLayout>
               </AdminRoute>
               <AdminRoute path="/admin/place" exact>
@@ -134,27 +130,27 @@ function App() {
               </AdminRoute>
               <AdminRoute path="/book/:id" exact>
                 <MainLayout>
-                    <OrderDetail />
+                  <OrderDetail />
                 </MainLayout>
               </AdminRoute>
               <Route path="/sign-up">
-                  <AppLayout>
-                      <Signup />
-                  </AppLayout>
-                </Route>
-                <Route path="/sign-in" exact>
-                  <AppLayout>
-                      <Signin />
-                  </AppLayout>
-                </Route>
-                <Route path="/logout">
-                  <AppLayout>
-                      <LogOut />
-                  </AppLayout>
-                </Route>
-                <Route path="*">
-                    <PageNotFound />
-                </Route>
+                <AppLayout>
+                  <Signup />
+                </AppLayout>
+              </Route>
+              <Route path="/sign-in" exact>
+                <AppLayout>
+                  <Signin />
+                </AppLayout>
+              </Route>
+              <Route path="/logout">
+                <AppLayout>
+                  <LogOut />
+                </AppLayout>
+              </Route>
+              <Route path="*">
+                <PageNotFound />
+              </Route>
             </Switch>
           </PrevFilterContext>
         </Route>
