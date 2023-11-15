1c27a7aead9a8a87b0654ad17438fc01aa607996
--add <jason.nixathor@obello.com>
2023-11-15 21:00:03 +0700

    add url for frontend

--- frontend/src/Components/RoomGallery/Image.js
+++ frontend/src/Components/RoomGallery/Image.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function Image({src,...rest}) {
-  src = src && src.includes('https://')
-    ? src
-    : 'http://localhost:5000/'+src;
-  return (
-    <img {...rest} src={src} alt={''} />
-  );
+export default function Image({ src, ...rest }) {
+  src =
+    src && src.includes("https://")
+      ? src
+      : "https://travelgo-a9qu.onrender.com/" + src;
+  return <img {...rest} src={src} alt={""} />;
 }
