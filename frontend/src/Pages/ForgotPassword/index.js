03f9346eaa91d91d28de8945ffb9336195666668
20120234 <20120234@student.hcmus.edu.vn>
2023-11-15 11:17:28 +0700

    fix: fix user email validating behavior and async updating on UI

--- frontend/src/Pages/ForgotPassword/index.js
+++ frontend/src/Pages/ForgotPassword/index.js
@@ -12,7 +12,7 @@ function ForgotPassword(props) {
     email: '',
   };
 
-  const [initial, setInitial] = useState(true);
+  const [initial, setInitial] = useState(false);
   const [email, setEmail] = useState('');
   const [userCheck, setUserCheck] = useState(true);
   const [confirm, setConfirm] = useState(false);
@@ -39,18 +39,23 @@ function ForgotPassword(props) {
   };
 
   useEffect(() => {
-    if(initial) return () => {
-      setInitial(false);
-    };
-   
-    if (forgot.error) {
-      setUserCheck(false);
+    setInitial(true);
+  }, [email]);
+
+  useEffect(() => {
+    if (!initial) {
+      if (forgot.loading && !forgot.error) {
+        setConfirm(true);
+        setUserCheck(true);
+      }
     } else {
-      setUserCheck(true);
-      setConfirm(true);
+      setInitial(false);
     }
-  },
-    [forgot])
+    return () => { 
+      setUserCheck(false);
+    };
+
+  }, [forgot]);
 
   return (
     <div className="ForgotPass my-5 py-3">
@@ -66,52 +71,51 @@ function ForgotPassword(props) {
         </div>
       </div>
 
-      {
-        confirm ?
-          <div className='text-center w-100 text-[#A20300]'>
-            <p>
-              Confirmation link has been sent to mail: <b>{email} </b> <br />
-              Open your email to activate the latest link and continue to log in then.
-            </p>
-          </div>
-          :
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ errors, touched }) => (
+      {confirm ?
+        <div className='text-center w-100 text-[#A20300]'>
+          <p>
+            Confirmation link has been sent to mail: <b>{email} </b> <br />
+            Open your email to activate the latest link and continue to log in then.
+          </p>
+        </div>
+        :
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
 
-              <Form>
-                <h3 style={{ textAlign: "center", marginBottom: "15px" }}>Forgot Password</h3>
-                <div className="form-holder forgot-pass-form">
-                  <div className={userCheck ? 'd-none' : 'd-block text-danger mb-2'}>Email: <b>{email}</b> is yet to be registered!</div>
-                  <Field
-                    name="email"
-                    type="text"
-                    className={
-                      'forgot-form-control' +
-                      (errors.email && touched.email ? ' is-invalid' : '')
-                    }
-                    placeholder="Input email"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
+            <Form>
+              <h3 style={{ textAlign: "center", marginBottom: "15px" }}>Forgot Password</h3>
+              <div className="form-holder forgot-pass-form">
+                <div className={userCheck ? 'd-none' : 'd-block text-danger mb-2'}>Email: <b>{email}</b> is yet to be registered!</div>
+                <Field
+                  name="email"
+                  type="text"
+                  className={
+                    'forgot-form-control' +
+                    (errors.email && touched.email ? ' is-invalid' : '')
+                  }
+                  placeholder="Input email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
 
-                <div className="form-button">
-                  <button
-                    className='forgot-pass-btn'
-                    type="submit">
-                    Confirm
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
+              <div className="form-button">
+                <button
+                  className='forgot-pass-btn'
+                  type="submit">
+                  Confirm
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       }
 
     </div>
