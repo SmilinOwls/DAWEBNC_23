96b8aafcafb35515f5e0e5586ca148a82d6cfa4e
20120234 <20120234@student.hcmus.edu.vn>
2023-12-03 17:33:20 +0700

    chore: add small change

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getShowCart } from "../../Actions/SidebarAction";
 import Wishlist from "../Wishlist";
 import { BiBook } from "react-icons/bi";
-import logo from "../../Assets/images/Logo.jpg";
+import logo from "../../Assets/images/Logo.jpeg";
 const nav_links = [
   {
     path: "/",
