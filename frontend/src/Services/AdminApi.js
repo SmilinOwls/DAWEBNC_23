dfdc99e33a057797a22e7dc80f51c6cbc472043c
truong pham <duytruong5437@gmail.com>
2024-01-09 10:00:29 +0700

    map student id

--- frontend/src/Services/AdminApi.js
+++ frontend/src/Services/AdminApi.js
@@ -26,105 +26,17 @@ const adminApi = {
             throw error;
         }
     },
-    async getAllPlaces() {
+    async getAllClassesByUserId(id) {
         try {
-            const response = await axios.get(`/place`);
+            const response = await axios.get(`/classroom/teacher/${id}`, configAxios);
             return response.data;
         } catch (error) {
             throw error;
         }
     },
-    async getPlaceById(id) {
-        try {
-            const response = await axios.get(`/place/${id}`, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async addPlace(place) {
-        try {
-            const response = await axios.post(`/place/admin`,place, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async updatePlace(place){
-        try {
-            const response = await axios.put(`/place/admin/${place._id}`, place, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async deletePlace(id){
-        try {
-            const response = await axios.delete(`/place/admin/${id}`, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async getAllRooms() {
+    async getClassByCreatedUser(id) {
         try {
             const response = await axios.get(`/room`, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async addRoom(room) {
-        try {
-            const response = await axios.post(`/room/admin`,room, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async updateRoom(room){
-        try {
-            const response = await axios.put(`/room/admin/${room._id}`,room, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async deleteRoom(id){
-        try {
-            const response = await axios.delete(`/room/admin/${id}`, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async getAllBlogs() {
-        try {
-            const response = await axios.get(`/blog`, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async addBlog(blog) {
-        try {
-            const response = await axios.post(`/blog/admin`,blog, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async updateBlog(blog){
-        try {
-            const response = await axios.put(`/blog/admin/${blog._id}`,blog, configAxios);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-    async deleteBlog(id){
-        try {
-            const response = await axios.delete(`/blog/admin/${id}`, configAxios);
             return response.data;
         } catch (error) {
             throw error;
